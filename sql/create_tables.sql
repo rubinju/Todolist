7f66d3dc10f644f520d5cea1e75cb14031144c79
Rufus <rufus.gylling@gmail.com>
2016-11-22 23:55:35 +0200

    changed order

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,21 +1,8 @@
-CREATE TABLE Task(
-	id SERIAL PRIMARY KEY,
-	description varchar(255) NOT NULL,
-	created DATE,
-	status boolean DEFAULT FALSE,
-	person INTEGER REFERENCES Person(id),
-	priority INTEGER REFERENCES Priority(id)
-);
 
 CREATE TABLE Person(
 	id SERIAL PRIMARY KEY, 
 	username varchar(50) NOT NULL,
 	password varchar(50) NOT NULL
-);
-
-CREATE TABLE Projects( -- TODO: check if it goes like this
-	task INTEGER REFERENCES Task(id),
-	project INTEGER REFERENCES Project(id)
 );
 
 CREATE TABLE Project(
@@ -24,7 +11,21 @@ CREATE TABLE Project(
 	person INTEGER REFERENCES Person(id)
 );
 
+CREATE TABLE Projects( -- TODO: check if it goes like this
+	task INTEGER REFERENCES Task(id),
+	project INTEGER REFERENCES Project(id)
+);
+
 CREATE TABLE Priority(
 	id SERIAL PRIMARY KEY,
 	name smallint NOT NULL
+);
+
+CREATE TABLE Task(
+	id SERIAL PRIMARY KEY,
+	description varchar(255) NOT NULL,
+	created DATE,
+	status boolean DEFAULT FALSE,
+	person INTEGER REFERENCES Person(id),
+	priority INTEGER REFERENCES Priority(id)
 );
