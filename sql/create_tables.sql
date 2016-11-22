a293cc7107a17195c93627e1df970bdf0a4d56d0
Rufus <rufus.gylling@gmail.com>
2016-11-22 23:34:34 +0200

    updated documentation & create table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,30 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Task(
+	id SERIAL PRIMARY KEY,
+	description varchar(255) NOT NULL,
+	created DATE,
+	status boolean DEFAULT FALSE,
+	user INTEGER REFERENCES User(id),
+	priority INTEGER REFERENCES Priority(id)
+);
+
+CREATE TABLE User(
+	id SERIAL PRIMARY KEY, 
+	username varchar(50) NOT NULL,
+	password varchar(50) NOT NULL
+);
+
+CREATE TABLE Projects( -- TODO: check if it goes like this
+	Task INTEGER REFERENCES Task(id),
+	Project INTEGER REFERENCES Project(id)
+);
+
+CREATE TABLE Project(
+	id SERIAL PRIMARY KEY,
+	name varchar(255) NOT NULL,
+	user INTEGER REFERENCES User(id)
+);
+
+CREATE TABLE Priority(
+	id SERIAL PRIMARY KEY,
+	name smallint NOT NULL
+);
