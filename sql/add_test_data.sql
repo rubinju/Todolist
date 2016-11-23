0aa7b51ee3f216e34decbdb840670993da9dbc2e
Rufus <rufus.gylling@gmail.com>
2016-11-23 20:38:05 +0200

    added testdata

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,16 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+-- Person table testdata --
+INSERT INTO Person (username, password) VALUES ('foo', 'bar');
+INSERT INTO Person (username, password) VALUES ('spaceball', '1234');
+
+-- Project table testdata --
+INSERT INTO Project (name) VALUES ('My summer car');
+INSERT INTO Project (name) VALUES ('Brewery');
+
+-- Priority table testdata --
+INSERT INTO Priority (name) VALUES ('0');
+INSERT INTO Priority (name) VALUES ('1');
+INSERT INTO Priority (name) VALUES ('2');
+INSERT INTO Priority (name) VALUES ('3');
+
+-- Task table testdata --
+INSERT INTO Task (description, created, status) VALUES ('Buy beer', '2016-11-20', 'false');
