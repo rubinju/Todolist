5c7aa999f8f6fb0874b42091874783735080ff0b
Rufus <rufus.gylling@gmail.com>
2016-12-04 14:33:18 +0200

    new method store()

--- app/controllers/tasks_controller.php
+++ app/controllers/tasks_controller.php
@@ -4,7 +4,20 @@
 		
 		public static function index() {
 			$tasks = Task::all(); //Get all tasks from db
-			View::make('task/index.html', array('tasks' => $tasks)); // TODO task/index.. does not exist there yet! See /tasklist and suunn../list.html
+			View::make('task/index.html', array('tasks' => $tasks));
+		}
+
+		public static function store() { // What do we do about parms that need to be put in a different db-table? Or creation-date that's not user submitted?
+			$params = $_POST;
+			$task = new Task(array(
+				'description' => $params['description'],
+				'priority' => $params['priority'],
+				'status' => $params['status']
+			));
+
+			$task->save(); // Tell task-model to save this object to DB
+
+			Redirect::to('/task/' . $task->id, array('message' => 'Task added to database!')); 
 		}
 	}
 
