0b0e3f0c382d593f9a8a97dcd0eb4f9b43a4cef1
Rufus <rufus.gylling@gmail.com>
2016-12-06 21:30:24 +0200

    validate errors on store()

--- app/controllers/tasks_controller.php
+++ app/controllers/tasks_controller.php
@@ -9,17 +9,25 @@
 
 		public static function store() { // What do we do about parms that need to be put in a different db-table? Or creation-date that's not user submitted?
 			$params = $_POST;
-			$task = new Task(array(
+			$attributes = array(
 				'description' => $params['description'],
 				'priority' => $params['priority']
 				//'status' => $params['status'] // this doesn't get relayed from new.html
-			));
+			);
+
+			$task = new Task($attributes);
+			$errors = $task->errors(); // Calls all validators
 
 			//Kint::dump($params); // Debug, comment out Redirect if used!
 
-			$task->save(); // Tell task-model to save this object to DB
+			if (count($errors) == 0) {
+				$task->save(); // Tell task-model to save this object to DB
 
-			Redirect::to('/task/' . $task->id, array('message' => 'Task added to database!')); 
+			Redirect::to('/task/' . $task->id, array('message' => 'Task added to database!'));
+			} else {
+				View::make('task/new.html', array('errors' => $errors, 'attributes' => $attributes));
+			}
+
 		}
 
 		public static function show($id) {
