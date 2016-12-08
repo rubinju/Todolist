847f1575bb3eb10f6689a28d261707b9c78ecea4
Rufus <rufus.gylling@gmail.com>
2016-12-08 19:43:12 +0200

    all, destroy

--- app/models/project.php
+++ app/models/project.php
@@ -1,3 +1,31 @@
 <?php
+	class Project extends BaseModel {
+		public $id, $name, $person;
 
+		public function __construct($attributes) {
+			parent::__construct($attributes);
+			//$this->validators = array('validate_description', 'validate_priority'); // TODO: add other validators
+		}
+
+		public static function all(){ 
+			$query = DB::connection()->prepare('SELECT * FROM Project WHERE person = :person');
+			$query->execute(array('person' => $_SESSION['user'])); // does not work with booleans! check out bindValue
+			$rows = $query->fetchAll();
+			$tasks = array();
+
+			foreach ($rows as $row) {
+				$projects[] = new Project(array(
+					'id' => $row['id'],
+					'name' => $row['name']
+				));
+			}
+			return $projects;
+		}
+
+		public function destroy() {
+			$query = DB::connection()->prepare('DELETE FROM Project WHERE id = :id');
+			$query->execute(array('id' => $this->id));
+			$row = $query->fetch();
+		}
+	}
 ?>
