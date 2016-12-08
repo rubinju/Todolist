78a72f6eafd5946bc1b7f67151ffa801d79bb7a1
Rufus <rufus.gylling@gmail.com>
2016-12-08 19:42:29 +0200

    index create destroy

--- app/controllers/project_controller.php
+++ app/controllers/project_controller.php
@@ -1,3 +1,23 @@
 <?php
+	class ProjectController extends BaseController	{
+		
+		public static function index() {
+			self::check_logged_in();
+			$projects = Project::all();
+			View::make('project/index.html', array('projects' => $projects));
+		}
 
+		public static function create() {
+			self::check_logged_in();
+			View::make('project/new.html');
+		}
+
+		public static function destroy($id) {
+			self::check_logged_in();
+			$project = new Project(array('id' => $id));
+			$project->destroy();
+			Redirect::to('/project', array('message' => 'Project removed successfully'));
+		}
+
+	}
 ?>
